968ad0e44827eb57b7e39f57d6e49c4aab68239b
Gedevald <tyta-hell@mail.ru>
2019-01-25 19:48:23 +0200

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,33 @@
+// что-то
+'use strict';
+/*
+
+*/
+var one = 1;
+console.log(one);
+
+let person = {
+	name: "John",
+	age: 25,
+	isMarried: false
+};
+
+console.log(person.age);
+console.log(person["name"]);
+
+let arr = ['plim.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[0]);
+
+//alert("Hello World");
+
+/* let answer1 = confirm("You learn js?");
+console.log(answer1); //true & false - boolean
+console.log(typeof(answer1)); */
+
+/*  let answer2 = prompt("Есть ли вам 18 лет?", "Да");
+console.log(answer2); //строка ввода - string */
+
+/*
+let answer2 = +prompt("Есть ли вам 18 лет?", "Да"); //+ перед string делает его number
+console.log(answer2); //строка ввода - number
+*/
